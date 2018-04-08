558491be6444a3f7b3cd44e90ffdf525eeaedbb1
Pablo Garcia <pablo@pablogs.io>
2018-04-08 07:55:16 +0200

    Optimizing TubeLineStop

--- TubeLineStops/TubeLineStops/TubeLineView/TubeLineStop.swift
+++ TubeLineStops/TubeLineStops/TubeLineView/TubeLineStop.swift
@@ -65,17 +65,17 @@ class TubeLineStop: UIView {
         case .tailRight:
             context?.move(to: CGPoint(x: 0, y: yLinePosition))
             context?.addLine(to: CGPoint(x: width/2, y: yLinePosition))
-        case .tailForkedLefStoptDown:
+        case .tailForkedLefStoptDown, .forkedStopEndDown:
+            var xMoveTo = width/2
+            var xAddLine = width
+            if positionInTheLine == .forkedStopEndDown {
+                xMoveTo = 0
+                xAddLine = width/2
+            }
             context?.move(to: CGPoint(x: 0, y: yLinePosition))
             context?.addLine(to: CGPoint(x: width, y: yLinePosition))
-            context?.move(to: CGPoint(x: width/2, y: height-circleDiameter/2))
-            context?.addLine(to: CGPoint(x: width, y: height-circleDiameter/2))
-            yStopPosition = height-circleDiameter
-        case .forkedStopEndDown:
-            context?.move(to: CGPoint(x: 0, y: yLinePosition))
-            context?.addLine(to: CGPoint(x: width, y: yLinePosition))
-            context?.move(to: CGPoint(x: 0, y: height-circleDiameter/2))
-            context?.addLine(to: CGPoint(x: width/2, y: height-circleDiameter/2))
+            context?.move(to: CGPoint(x: xMoveTo, y: height-circleDiameter/2))
+            context?.addLine(to: CGPoint(x: xAddLine, y: height-circleDiameter/2))
             yStopPosition = height-circleDiameter
         case .middle:
             context?.move(to: CGPoint(x: 0, y: yLinePosition))
