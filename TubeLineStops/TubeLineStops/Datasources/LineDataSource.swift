c289e726429f874309f0941c926f13e78186e7e0
Pablo Garcia <pablo@pablogs.io>
2018-04-08 08:09:45 +0200

    Naming tube stops

--- TubeLineStops/TubeLineStops/Datasources/LineDataSource.swift
+++ TubeLineStops/TubeLineStops/Datasources/LineDataSource.swift
@@ -18,17 +18,16 @@ class LineDataSource: NSObject, UICollectionViewDataSource {
     
     var lineStops: [LineStop] {
         var stops = [LineStop]()
-        let stop01 = LineStop(name: "Ealing Brodway", type: TubeLineStop.StopType.tailLeft)
-        let stop02 = LineStop(name: "02", type: TubeLineStop.StopType.tailForkedLefStoptDown)
-        let stop03 = LineStop(name: "03", type: TubeLineStop.StopType.unionRightStopUp)
-        let stop04 = LineStop(name: "04", type: TubeLineStop.StopType.middle)
-        let stop05 = LineStop(name: "05", type: TubeLineStop.StopType.forkLeftStopDown)
-        let stop06 = LineStop(name: "06", type: TubeLineStop.StopType.forkedStopUp)
-        let stop07 = LineStop(name: "07", type: TubeLineStop.StopType.forkedStopEndDown)
-        let stop08 = LineStop(name: "08", type: TubeLineStop.StopType.middle)
-        let stop09 = LineStop(name: "08", type: TubeLineStop.StopType.middle)
-        
-        let stop10 = LineStop(name: "09", type: TubeLineStop.StopType.tailRight)
+        let stop01 = LineStop(name: "High Barnet", type: TubeLineStop.StopType.tailLeft)
+        let stop02 = LineStop(name: "Mill Hill East", type: TubeLineStop.StopType.tailForkedLefStoptDown)
+        let stop03 = LineStop(name: "Woodside Park", type: TubeLineStop.StopType.unionRightStopUp)
+        let stop04 = LineStop(name: "Highgate", type: TubeLineStop.StopType.middle)
+        let stop05 = LineStop(name: "Liecester Square", type: TubeLineStop.StopType.forkLeftStopDown)
+        let stop06 = LineStop(name: "Old Street", type: TubeLineStop.StopType.forkedStopUp)
+        let stop07 = LineStop(name: "Ealing Broadway", type: TubeLineStop.StopType.forkedStopEndDown)
+        let stop08 = LineStop(name: "Greenford", type: TubeLineStop.StopType.middle)
+        let stop09 = LineStop(name: "Winbledon", type: TubeLineStop.StopType.middle)
+        let stop10 = LineStop(name: "Paddington", type: TubeLineStop.StopType.tailRight)
         
         stops.append(stop01)
         stops.append(stop02)
